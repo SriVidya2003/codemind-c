1cda0923a4c323b1148649228523651e982a4621
SriVidya2003 <98937360+SriVidya2003@users.noreply.github.com>
2022-05-02 17:45:47 +0100

    #include<stdio.h> void fibonacci(int,int,int); int main() {     int n1=0,n2=1,n3,n,i;     scanf("%d",&n);     fibonacci(n1,n2,n);     return 0; } void fibonacci(int n1,int n2,int n) {     int i,n3,sum=0;     for(i=3;i<=n;i++)     {         n3=n1+n2;         if(n3==n)         {             sum++;         }         n1=n2;         n2=n3;     }     if(sum>0)     printf("True");     else     printf("False"); }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
+void fibonacci(int,int,int);
 int main()
 {
- int a,b,c,next,num;
- scanf("%d", &num);
- if((num==0)||(num==1))
-  printf("True");
- else
- {
-   a=0;
-   b=1;
-   c=a+b;
-   while(c<num)
-   {
-     a=b;
-     b=c;
-     c=a+b;
-   }
-   if(c==num)
-     printf("True");
-   else
-     printf("False");
- }
- return 0;
+    int n1=0,n2=1,n3,n,i;
+    scanf("%d",&n);
+    fibonacci(n1,n2,n);
+    return 0;
+}
+void fibonacci(int n1,int n2,int n)
+{
+    int i,n3,sum=0;
+    for(i=3;i<=n;i++)
+    {
+        n3=n1+n2;
+        if(n3==n)
+        {
+            sum++;
+        }
+        n1=n2;
+        n2=n3;
+    }
+    if(sum>0)
+    printf("True");
+    else
+    printf("False");
 }
