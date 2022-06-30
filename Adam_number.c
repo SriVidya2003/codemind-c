10bedc67c221406fe66d507c739f0e7c0fb019c0
SriVidya2003 <98937360+SriVidya2003@users.noreply.github.com>
2022-06-30 06:05:26 +0100

    #include<stdio.h> int main() {     int n,r,s,sum=0,pro=0,temp,a=0;     scanf("%d",&n);     s=n*n;     temp=n;     while(temp>0)     {         r=temp%10;         sum=sum*10+r;         temp=temp/10;     }     pro=sum*sum;     while(pro>0)     {         r=pro%10;         a=a*10+r;         pro=pro/10;     }     if(a==s)     {         printf("True");     }     else     printf("False");     return 0; }

--- Adam_number.c
+++ Adam_number.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,s,sum=0,prod=0,temp,a=0;
+    int n,r,s,sum=0,pro=0,temp,a=0;
     scanf("%d",&n);
     s=n*n;
     temp=n;
@@ -11,16 +11,18 @@ int main()
         sum=sum*10+r;
         temp=temp/10;
     }
-    prod=sum*sum;
-    while(prod>0)
+    pro=sum*sum;
+    while(pro>0)
     {
-    r=prod%10;
-    a=a*10+r;
-    prod=prod/10;
-}
-if(a==s)
-printf("True");
-else
-printf("False");
-return 0;
+        r=pro%10;
+        a=a*10+r;
+        pro=pro/10;
+    }
+    if(a==s)
+    {
+        printf("True");
+    }
+    else
+    printf("False");
+    return 0;
 }
